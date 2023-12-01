febed155e848628653d803a2501d199517232b35
Mark Kruse <1224221+krusem@users.noreply.github.com>
2023-12-01 10:16:50 -0500

    Update day.ps1

--- day1/day.ps1
+++ day1/day.ps1
@@ -1,6 +1,7 @@
-function day1($a) {
+function day1($f) {
+  $a = [IO.File]::ReadAllLines($f).split("`n")
   $t = 0
-  $a | %{
+    $a | %{
     $m = $_ -replace "[^0-9]" , ''
     $t += [int]::Parse($m[0])*10 + [int]::Parse($m[-1])
   }
